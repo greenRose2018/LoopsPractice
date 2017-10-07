7b45e45bf1f0ca52d1e1d2dc4929c4520112c9d7
greenRose2018 <brifun13@gmail.com>
2017-10-07 18:50:47 -0400

    added the list, made list and sample pancake.
    no bugs and functionally properly

--- src/useful/controller/UsefulController.java
+++ src/useful/controller/UsefulController.java
@@ -7,11 +7,30 @@ import java.util.ArrayList;
 
 public class UsefulController
 {
+	private List<Pancake> pancakeList;
+	private PopupDisplay display;
+	
+	
 	public UsefulController()
 	{
+		pancakeList = new ArrayList<Pancake>();
+		display = new PopupDisplay();
 		
 	}
 	public void start()
+	{
+		display.displayText("hello");
+		Pancake sample = new Pancake();
+		display.displayText(sample.toString());
+		
+	}
+	
+	private void showTheList()
+	{
+		
+	}
+	
+	private void fillTheList()
 	{
 		
 	}
