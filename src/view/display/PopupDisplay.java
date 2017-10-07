9094684534dd1ed6880982e55287a8f450f967c9
greenRose2018 <brifun13@gmail.com>
2017-10-07 16:57:23 -0400

    finished setup for popup display

--- src/view/display/PopupDisplay.java
+++ src/view/display/PopupDisplay.java
@@ -5,5 +5,18 @@ import javax.swing.JOptionPane;
 public class PopupDisplay
 {
 	// loop up listPractice for help
+	public void displayText(String textToDisplay)
+	{
+		JOptionPane.showMessageDialog(null, textToDisplay);
+	}
+	
+	public String getResponse(String questionAsked)
+	{
+		String response = "";
+		
+		response += JOptionPane.showInputDialog(null, questionAsked);
+		
+		return response;
+	}
 	
 }
