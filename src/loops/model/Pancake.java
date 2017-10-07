0614c6ea0ce31372636170a37b763a63325d52b4
greenRose2018 <brifun13@gmail.com>
2017-10-07 17:29:30 -0400

    data memders set

--- src/loops/model/Pancake.java
+++ src/loops/model/Pancake.java
@@ -2,5 +2,14 @@ package loops.model;
 
 public class Pancake
 {
+	private String flavor;
+	private String shape;
+	private boolean sweet;
+	private boolean toppingsSyrup;
+	private int numFruits; 
 	
+	private Pancake()
+	{
+		
+	}
 }
