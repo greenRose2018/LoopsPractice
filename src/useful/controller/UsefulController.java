6257e3bc8dda6c78267a61c1cfdd098b70544291
greenRose2018 <brifun13@gmail.com>
2017-10-08 15:52:48 -0400

    added helper method/ try and catch and also getResponse for numPancakes

--- src/useful/controller/UsefulController.java
+++ src/useful/controller/UsefulController.java
@@ -40,4 +40,38 @@ public class UsefulController
 		String favPancake = display.getResponse("Mr.Fluff wants to know what flavor of pancake you like?");
 		display.displayText("You like " + favPancake + " pancake!!");
 		display.displayText("Now, Mr.Fluff wants you to make your own pancake!!");
+		
+		int specialAnswer = 0;
+		String numOfPancake = display.getResponse("How many pancakes do you want?");
+		while(!isValidInteger(numOfPancake))
+		{
+			display.displayText("Wrong, Try Again!");
+			numOfPancake = display.getResponse("How many pancakes?");
+		}
+		specialAnswer = Integer.parseInt(numOfPancake);
+		int numPan = 0;
+		numPan = specialAnswer;
+		
+		if(numPan < 0)
+		{
+			display.displayText("You can't put negative numbers, silly person.");
+		}
+		
+	//helper methods
+	private boolean isValidInteger(String waffle)
+	{
+		boolean valid = false;
+		
+		try
+		{
+			Integer.parseInt(waffle);
+			valid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			display.displayText("Only integervalues are valid: " + waffle + "is not valid");
+		}
+		
+		return valid;
+	}
 }
